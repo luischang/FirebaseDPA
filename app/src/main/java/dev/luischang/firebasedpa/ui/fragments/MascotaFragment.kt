ec8a39e58347e540052c4c4b66e7a2d48e2d6973
Luis Chang <luis.chang@outlook.com>
2021-06-19 17:23:13 -0500

    Mascota Fragment finish v1

--- app/src/main/java/dev/luischang/firebasedpa/ui/fragments/MascotaFragment.kt
+++ app/src/main/java/dev/luischang/firebasedpa/ui/fragments/MascotaFragment.kt
@@ -1,6 +1,7 @@
 package dev.luischang.firebasedpa.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,12 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dev.luischang.firebasedpa.R
+import dev.luischang.firebasedpa.ui.fragments.adapter.MascotaAdapter
+import dev.luischang.firebasedpa.ui.fragments.client.MascotaClient
+import dev.luischang.firebasedpa.ui.fragments.model.MascotaModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MascotaFragment : Fragment() {
 
@@ -22,8 +29,23 @@ class MascotaFragment : Fragment() {
 
         rvmascotas.layoutManager = LinearLayoutManager(requireActivity())
 
-        
+        val call: Call<List<MascotaModel>> = MascotaClient
+                .retrofitService.listarMascota()
+        call.enqueue(object: Callback<List<MascotaModel>>{
+            override fun onResponse(
+                call: Call<List<MascotaModel>>,
+                response: Response<List<MascotaModel>>
+            ) {
+                Log.d("onResponse",response.body().toString())
+                rvmascotas.adapter = MascotaAdapter(response.body()!!)
+            }
 
+            override fun onFailure(call: Call<List<MascotaModel>>, t: Throwable) {
+                Log.d("OnFailure","Ocurrió un error en retrofitService.listarMascota()")
+            }
+
+
+        })
 
 
 
